f97b85b7b70f69254a063f66c1d386ccdd5d72df
Flashw0rk <1flashw@gmail.com>
2025-10-27 00:17:38 +0400

    RELEASE: Update version to 1.0 for release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.example.vkbookandroid"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "2.3"
+        versionCode = 1
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
